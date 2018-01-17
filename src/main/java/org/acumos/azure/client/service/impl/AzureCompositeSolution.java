2dd39e202ec06cf10bda3bbc547950b778b3fc3f
Santosh Kumar18 <SK00472486@TechMahindra.com>
2018-01-17 21:47:06 +0530

    Add dockerVmUser and password
    Change-Id: I9ad1abe63d77e3bf8e66ce472c2d8b295745ea14
    Signed-off-by: Santosh Kumar18 <SK00472486@TechMahindra.com>

--- src/main/java/org/acumos/azure/client/service/impl/AzureCompositeSolution.java
+++ src/main/java/org/acumos/azure/client/service/impl/AzureCompositeSolution.java
@@ -462,8 +462,10 @@ public class AzureCompositeSolution implements Runnable {
 			    		            	}
 			    		            	
 			    		            	String azureVMIP=azureBean.getAzureVMIP();
-			    		            	final String vmUserName="dockerUser";
-			    		        		final String vmPassword="12NewPA$$w0rd!";
+			    		            	//final String vmUserName="dockerUser";
+			    		        		//final String vmPassword="12NewPA$$w0rd!";
+			    		        		final String vmUserName=dockerVMUserName;
+			    		        		final String vmPassword=dockerVMPassword;
 			    		        		String repositoryName="";
 			    		        		repositoryName=privateRepoUrl+":"+tagImage;
 			    		        		String portNumber="";
